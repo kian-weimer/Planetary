0390c5a10bd8b7959900b5072877b226c9f82934
Kian Weimer <kianweimer@gmail.com>
2020-10-06 10:52:42 -0500

    fixed resource pick up

--- Assets/Scripts/resource.cs
+++ Assets/Scripts/resource.cs
@@ -8,19 +8,20 @@ public class Resource : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Pickedup()
     {
         Debug.Log("ahhh");
         FindObjectOfType<canvas>().transform.Find("ItemImage").GetComponent<Image>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
-        //Destroy(gameObject);
+        transform.position = new Vector3(0, 0, 1);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
     }
 }
