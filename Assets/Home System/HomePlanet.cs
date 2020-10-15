9276ca869e6605f36e89960e1c78a4b55e14b4e3
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-10-14 23:44:22 -0500

    cleaned up organization

---
+++ Assets/Home System/HomePlanet.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomePlanet : MonoBehaviour
+{
+    public List<(GameObject, int)> items;
+    public int numberOfItemSlots = 3;
+    public string name;
+
+    public GameObject planetHUD;
+
+    // a list of the items that the planet is producing
+    // (item, amount produced, frequency (seconds))
+    public List<ProductionItem> productionItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planetHUD = transform.parent.GetComponent<Home>().planetHUD;
+        items = new List<(GameObject, int)>();
+        for (int item = 0; item < numberOfItemSlots; item++)
+        {
+            items.Add((null, 0));
+        }
+
+    }
+
+    public void UpdateUI()
+    {
+        for (int item = 0; item < numberOfItemSlots; item++)
+        {
+            planetHUD.transform.Find("Item" + (item + 1)).Find("Quantity").gameObject.GetComponent<Text>().text = "X" + (items[item].Item2 + "").PadLeft(2, '0');
+        }   
+    }
+
+    // add a quantity of an item to a specified item slot
+    // returns true if successful, false otherwise
+    public bool addItem(GameObject item, int quantity, int itemSlot)
+    {
+        if (items[itemSlot].Item1 == null)
+        {
+            items[itemSlot] = (item, quantity);
+            return true;
+        }
+        else if (items[itemSlot].Item1 == item)
+        {
+            items[itemSlot] = (item, items[itemSlot].Item2 + quantity);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool addItem(GameObject item, int quantity)
+    {
+        // loop through, check for item, add quantity if found
+        for (int itemSlot = 0; itemSlot < numberOfItemSlots; itemSlot++)
+        {
+            if (items[itemSlot].Item1 == item)
+            {
+                items[itemSlot] = (item, items[itemSlot].Item2 + quantity);
+                return true;
+            }
+        }
+
+        // loop through, find first null slot, add item and quantity if successful
+        for (int itemSlot = 0; itemSlot < numberOfItemSlots; itemSlot++)
+        {
+            if (items[itemSlot].Item1 == null)
+            {
+                items[itemSlot] = (item, quantity);
+                return true;
+            }
+        }
+
+        // no item slots available
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
