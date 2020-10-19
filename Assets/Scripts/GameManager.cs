fedeba1b720f28d2739b632e9a0731b63b151d84
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-10-19 15:03:54 -0500

    added startmenu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class GameManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void EnterGame()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
 
