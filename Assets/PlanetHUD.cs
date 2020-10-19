e1045c5d38ba182eb1757e118c395ebe4b124f9e
Kian Weimer <kianweimer@gmail.com>
2020-10-19 15:09:37 -0500

    Planet Adjustments

---
+++ Assets/PlanetHUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetHUD : MonoBehaviour
+{
+    public PlayerController player;
+    public AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        player.speed = 0;
+        audioManager.Stop("Thrust");
+    }
+}
