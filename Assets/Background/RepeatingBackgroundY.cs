3f847709df60ed51c96db901a01f0eaaf39b1a25
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-10-31 13:20:35 -0500

    assets added for moving background may be implemented later

---
+++ Assets/Background/RepeatingBackgroundY.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackgroundY : MonoBehaviour
+{
+    public GameObject leftMost;
+    public GameObject rightMost;
+    private BoxCollider2D boxCollide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollide = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move from top to bottom
+        if(FindObjectOfType<Player>().transform.position.y < transform.position.y - boxCollide.size.y)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 2f * boxCollide.size.y);
+        }
+
+        //move from bottom to top
+        if (FindObjectOfType<Player>().transform.position.y > transform.position.y + boxCollide.size.y)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 2f * boxCollide.size.y);
+        }
+    }
+}
---
+++ Assets/Background/ScrollingBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = player.GetComponent<Rigidbody2D>().velocity * new Vector2(-1,-1);
+    }
+}
