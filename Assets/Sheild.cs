a5d4f681fa8adc5b74645fd9ca3ede300e84c699
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-11-05 09:42:01 -0600

    working sheild

--- Assets/Sheild.cs
+++ Assets/Sheild.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Sheild : MonoBehaviour
 {
+    public GameObject shieldBar;
+    public float damageAmmountFromShip;
+    public float damageAmmountFromBullet;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
+            FindObjectOfType<Player>().sheild = FindObjectOfType<Player>().sheild - damageAmmountFromShip;
             collision.gameObject.GetComponent<EnemyController>().damage(collision.gameObject.GetComponent<EnemyController>().maxHealth);
+            shieldBar.GetComponent<RectTransform>().localScale = new Vector3(FindObjectOfType<Player>().sheild / FindObjectOfType<Player>().maxSheild,
+                shieldBar.GetComponent<RectTransform>().localScale.y,
+                shieldBar.GetComponent<RectTransform>().localScale.z);
+        }
+        if(collision.gameObject.tag == "enemyBullet")
+        {
+            FindObjectOfType<Player>().sheild = FindObjectOfType<Player>().sheild - damageAmmountFromBullet;
+            shieldBar.GetComponent<RectTransform>().localScale = new Vector3(FindObjectOfType<Player>().sheild / FindObjectOfType<Player>().maxSheild,
+                shieldBar.GetComponent<RectTransform>().localScale.y,
+                shieldBar.GetComponent<RectTransform>().localScale.z);
         }
     }
 }
