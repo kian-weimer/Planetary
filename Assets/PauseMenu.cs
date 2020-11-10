4179cbf491e27a1c5d95cbc684443f1871158891
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-11-10 12:03:53 -0600

    Revert "popup"
    This reverts commit 9208d7bb17f267c17f63e5e3e99ba943ae469831.

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -37,6 +37,7 @@ public class PauseMenu : MonoBehaviour
             state = (state + 1) % 2;
             button.GetComponent<Text>().text = states[state];
             pauseMenu.SetActive(!pauseMenu.activeSelf);
+            Debug.Log("Pause Toggled!");
             gameManager.togglePause();
             shopButton.SetActive(!shopButton.activeSelf);
         }
