959e65ea12393f657c383488d587504422ee43da
Rys Huehnergarth <rys-huehnergarth@uiowa.edu>
2020-12-11 18:28:43 -0600

    player skins/ tutorial

---
+++ Assets/Sun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().loseHealth(collision.gameObject.GetComponent<Player>().maxHealth);
+            collision.gameObject.GetComponent<Player>().isHome = false;
+        }
+    }
+}
